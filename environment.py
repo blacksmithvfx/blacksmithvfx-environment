81dd68f3e139847d73bd615fae621f2e71682802
Toke Jepsen <tokejepsen@gmail.com>
2016-12-14 21:08:33 +0100

    Working version of templates and environment

---
+++ environment.py
@@ -0,0 +1,26 @@
+import os
+
+from conda_git_deployment import utils
+
+
+environment = {}
+
+# FTRACK_TEMPLATES_PATH
+path = os.path.join(
+    os.path.join(
+        os.path.join(os.path.dirname(__file__)), "environment",
+        "FTRACK_TEMPLATES_PATH"
+    ),
+)
+
+environment["FTRACK_TEMPLATES_PATH"] = path
+
+# PYBLISHPLUGINPATH
+path = os.path.join(
+    os.environ["CONDA_GIT_REPOSITORY"], "pyblish-blacksmithvfx",
+    "pyblish_blacksmithvfx", "plugins", "maya"
+)
+
+environment["PYBLISHPLUGINPATH"] = path
+
+utils.write_environment(environment)
