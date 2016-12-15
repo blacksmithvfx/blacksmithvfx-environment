5d29b737215ca75048262bcf672f1cabcbb1e779
Toke Jepsen <tokejepsen@gmail.com>
2016-12-15 12:01:11 +0100

    Adding pyblish-blacksmithvfx/houdini, pyblish-deadline, pyblish-bumpybox/deadline, to environment

--- environment.py
+++ environment.py
@@ -16,9 +16,26 @@ path = os.path.join(
 environment["FTRACK_TEMPLATES_PATH"] = path
 
 # PYBLISHPLUGINPATH
-path = os.path.join(
-    os.environ["CONDA_GIT_REPOSITORY"], "pyblish-blacksmithvfx",
-    "pyblish_blacksmithvfx", "plugins", "maya"
+path = (
+    os.path.join(
+        os.environ["CONDA_GIT_REPOSITORY"], "pyblish-blacksmithvfx",
+        "pyblish_blacksmithvfx", "plugins", "maya"
+    ) +
+    os.pathsep +
+    os.path.join(
+        os.environ["CONDA_GIT_REPOSITORY"], "pyblish-blacksmithvfx",
+        "pyblish_blacksmithvfx", "plugins", "houdini"
+    ) +
+    os.pathsep +
+    os.path.join(
+        os.environ["CONDA_GIT_REPOSITORY"], "pyblish-deadline",
+        "pyblish_deadline", "plugins"
+    ) +
+    os.pathsep +
+    os.path.join(
+        os.environ["CONDA_GIT_REPOSITORY"], "pyblish-bumpybox",
+        "pyblish_bumpybox", "plugins", "deadline"
+    )
 )
 
 environment["PYBLISHPLUGINPATH"] = path
